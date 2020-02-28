377aa2ea1a97b15f925aaba09a197b9dc63d56fb
kevin.mcgregor <kevin.mcgregor@edgenuity.com>
2020-02-27 19:55:36 -0700

    updated Project info

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,3 +1,17 @@
+$(document).on("click", "#crystal", function () {
+    $('#main').empty();
+    const newDiv = $('<div>');
+    const newHref = $('<div>Project Link: <a href = "https://kevinroymcgregor.github.io/unit-4-game/" target="blank">Crystal Collector</a></div>')
+    const newP = $('<p>This project highlights JavaScript math functions, including random number generation, ' +
+        'and JavaScript functions.  A random value target is generated, and each crystal is assigned a random value as well. ' +
+        'The values assigned to the crystals are added together as they are clicked, with the goal being to reach  ' + 
+        'the target value without going over.  The program keeps a running total of wins and losses. ' +
+        'The site uses JavaScript alerts to display information.  The page is styled primarily using BootStrap.</p>')
+    newDiv.append(newHref);
+    newDiv.append(newP);
+    $('#main').append(newDiv);
+});
+
 $(document).on("click", "#GamR", function () {
     $('#main').empty();
     const newDiv = $('<div>');
@@ -14,16 +28,18 @@ $(document).on("click", "#GamR", function () {
     $('#main').append(newDiv);
 });
 
-$(document).on("click", "#crystal", function () {
+$(document).on("click", "#Retro", function () {
     $('#main').empty();
     const newDiv = $('<div>');
-    const newHref = $('<div>Project Link: <a href = "https://kevinroymcgregor.github.io/unit-4-game/" target="blank">Crystal Collector</a></div>')
-    const newP = $('<p>This project highlights JavaScript math functions, including random number generation, ' +
-        'and JavaScript functions.  A random value target is generated, and each crystal is assigned a random value as well. ' +
-        'The values assigned to the crystals are added together as they are clicked, with the goal being to reach  ' + 
-        'the target value without going over.  The program keeps a running total of wins and losses. ' +
-        'The site uses JavaScript alerts to display information.  The page is styled primarily using BootStrap.</p>')
+    const newHref = $('<div>Project Link: <a href = "https://retrotrade.herokuapp.com/" target="blank">Retro Trade</a></div>')
+    const newP = $('<p>This was the final project for our coding bootcamp. This was the first group ' +
+        'project where we picked our own group.  We decided to create a sales platform for vintage games. ' +
+        'The page is hosted on Heroku, and is written using React.  The data is stored in Mongo, ' +
+        'with the images stored in AWS. Users create a login with an email address and password. ' + 
+        'The password is encrypted using bcrypt to create a secure login process.  When items are ' + 
+        'purchased, they are flagged as not active in the MongoDB, and the system filters those results. </p>')
     newDiv.append(newHref);
     newDiv.append(newP);
     $('#main').append(newDiv);
 });
+
